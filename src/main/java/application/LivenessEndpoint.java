c4b1763ad7ae4194754a624e251579bae3154291
Jesus Matos <jesus.matosp@gmail.com>
2019-12-19 14:53:26 -0500

    comentario1

--- src/main/java/application/LivenessEndpoint.java
+++ src/main/java/application/LivenessEndpoint.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LivenessEndpoint {
 
+	// jmatos  2123
     @ReadOperation
     public String testLiveness() {
         return "{\"status\":\"UP\"}";
