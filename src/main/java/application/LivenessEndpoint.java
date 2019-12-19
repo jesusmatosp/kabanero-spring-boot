d566789f100e0466423427e52592ca0e45ae22af
Jesus Matos <jesus.matosp@gmail.com>
2019-12-19 17:44:40 -0500

    dmeo

--- src/main/java/application/LivenessEndpoint.java
+++ src/main/java/application/LivenessEndpoint.java
@@ -26,7 +26,7 @@ public class LivenessEndpoint {
 	// jmatos  2123
 	// Line two 22323
 	// demo
-	// demo 124213
+	// demo 124213sadsads
     @ReadOperation
     public String testLiveness() {
         return "{\"status\":\"UP\"}";
